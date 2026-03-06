fa6810813fe0f783ac0e79565d2514b48048b14a
Sainaresh-k <ksainaresh045@gmail.com>
2026-03-06 11:52:45 +0530

    Removed node modules and added gitignore

---
+++ README.md
@@ -0,0 +1,8 @@
+# Namasthe React
+
+This project is created while learning React.
+
+## Topics
+- React Basics
+- Components
+- JSX
---
+++ dist/Namasthe-React.de0c18c6.js
@@ -0,0 +1,2 @@
+const heading=React.createElement("h1",{},"Hello Sai...This is from React..!"),root=ReactDOM.createRoot(document.getElementById("root"));root.render(heading);
+//# sourceMappingURL=Namasthe-React.de0c18c6.js.map
