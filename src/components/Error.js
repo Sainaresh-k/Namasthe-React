9944da040a0f13ec94cda8cbf05d72d15b6b1ce2
Sainaresh-k <ksainaresh045@gmail.com>
2026-04-03 14:36:06 +0530

    feat:Error.js file added

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,13 @@
+import { useRouteError } from 'react-router-dom'
 const Error = () => {
+    const err = useRouteError();
+    console.log(err)
     return (
         <div>
             <h1>Oops...!!!</h1>
-            <h2 style={{ color: "red" }}>Something Went Wrong...!</h2>        </div>
+            <h2 style={{ color: "red" }}>Something Went Wrong...!</h2>
+            <h3>{err.status}-{err.statusText}</h3>
+        </div>
     )
 }
 export default Error;
