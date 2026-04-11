e250da01590e6224e5d7ec1b9856d3da29bff994
Sainaresh-k <ksainaresh045@gmail.com>
2026-04-11 09:41:12 +0530

    Day1

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,9 @@ const About = () => {
     return (
         <div className="about-container">
             <h2>this is About page</h2>
-            <User name={"SAI-fun"} loc={"hyd"} />
+            {/* <User name={"SAI-fun"} loc={"hyd"} /> */}
             <UserClass name={"Jassi-Class"} loc={"BNG"} />
         </div>
-
-
     )
 }
 export default About;
